dd5b82a37bf8e0e381026595e2f7febc3b3105c2
Quintravious Kelly <quintraviousk@gmail.com>
2023-09-12 19:57:10 -0500

    First Commit. Added the reverse method to main.java

--- src/edu/jsu/mcis/cs310/Main.java
+++ src/edu/jsu/mcis/cs310/Main.java
@@ -16,8 +16,17 @@ public class Main {
         return "Hello, World!";
     }
     
-    public String reverse(String message) {
-        return message;
+    public static String reverse(String input) {
+    // Create a StringBuilder to store the reversed string
+    StringBuilder reversed = new StringBuilder();
+
+    // Iterate through the characters of the input string in reverse order
+    for (int i = input.length() - 1; i >= 0; i--) {
+        reversed.append(input.charAt(i));
     }
-    
+
+    // Convert the StringBuilder to a String and return it
+    return reversed.toString();
+}
+
 }
